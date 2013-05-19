f66a5072ebd01e29d14b542a72b8a88d13f67023
Robert <hamstar@telescum.co.nz>
2013-05-19 17:28:22 +1200

    updated aliases

--- home/.zsh/aliases.sh
+++ home/.zsh/aliases.sh
@@ -1,6 +1,4 @@
 alias dug="du -sh --exclude=.git";
 alias mdp="mkdir -p";
 alias rmod="rspec -P spec/models/*";
-alias ssv="susvc";
-alias zhax="cd ~/src/zhax";
 alias fwds="cat ~/.ssh/config|grep -P 'Host |Forward' |grep -vP '^#' |sed 's/^Host /\n/'";
